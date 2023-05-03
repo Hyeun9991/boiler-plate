5a6d94b52ff6ae0825908de65f7ee0e44b529737
Hyeun9991 <hyeun9991@gmail.com>
2023-05-03 20:37:44 +0900

    mongoDB 연결

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! 안녕하세요'));
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    'mongodb+srv://eh123:123eh@node-b.8fnjs3a.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('MongoDB Connected...');
+  })
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World! 안녕하세요  '));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- record.md
+++ record.md
@@ -1,5 +1,15 @@
+# 1 express js app 만들기
+
 - npm init
 - index.js 생성
 - npm install express --save
 - express js 앱 만들기
-- start 명령어 만들기 `npm run start`
+- start 명령어 만들기 {명령어: `npm run start`}
+
+# 2 mongoDB 연결
+
+- mongoDB 클라스터 생성
+- mongoDB 유저 생성
+- mongoose 설치 {`npm install mongoose --save`}
+- mongoose.connect()
+- npm run start 테스트 - mongoDB 연결 확인
