6f797e88895c5fc7fbd0aa5d41007c54e400ab87
Hyeun9991 <hyeun9991@gmail.com>
2023-05-03 21:31:12 +0900

    회원가입 기능 구현, postman test

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 // backend server
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
+const { User } = require("./models/User");
 
-const mongoose = require('mongoose');
+// client에서 오는 정보를 server가 분석할 수 있게 설정
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const mongoose = require("mongoose");
 mongoose
   .connect(
-    'mongodb+srv://eh123:123eh@node-b.8fnjs3a.mongodb.net/?retryWrites=true&w=majority'
+    "mongodb+srv://eh123:123eh@node-b.8fnjs3a.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => {
-    console.log('MongoDB Connected...');
+    console.log("MongoDB Connected...");
   })
   .catch((err) => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World! 안녕하세요  '));
+app.get("/", (req, res) => res.send("Hello World! 안녕하세요  "));
+
+// 회원가입
+app.post("/register", async (req, res) => {
+  /**
+   * client에서 전달받은 data를 DB에 저장
+   *
+   * {
+   *    id: "eh",
+   *    password: "2u319"
+   * }
+   * req.body에는 이런식으로 데이터가 들어있음.
+   */
+
+  const user = new User(req.body); // 인스턴스 생성
+
+  // User Model에 저장
+  await user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({
+        success: false,
+        err: err,
+      });
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- record.md
+++ record.md
@@ -2,7 +2,7 @@
 
 - npm init
 - index.js 생성
-- npm install express --save
+- express 설치 {`npm install express --save`}
 - express js 앱 만들기
 - start 명령어 만들기 {명령어: `npm run start`}
 
@@ -19,3 +19,20 @@
 > Model: Schema 를 감싸주는 역할 / Schema: 데이터 정보를 지정
 
 - modules > User.js 생성 후 모델, 스키마 작성
+
+# 4. SSH를 이용해 GITHUB 연결
+
+- ssh-key 생성 {`ssh-keygen -t ed25519 -C "hyeun9991@gmail.com"`}
+- ssh agent를 background에 켜기 {`eval "(ssh-agent -s)"`}
+- ssh-pub key를 ssh-agent에 add {`ssh-add -K ~/.ssh/id_rsa`}
+- ssh-pub를 github에 연결 {`pbcopy < ~/.ssh/id_rsa.pub`}
+- github/setting/ssh and gpg keys에 생성한다음 복사 붙여넣기
+
+# 5. 회원 가입 기능 구현
+
+- body-parser 설치 {`npm install body-parser --save`}
+- postman 다운 [컴퓨터에]
+- body-parser 삭제 (해당 기능은 express에 기본적으로 포함되어있음.) {`npm uninstall body-parser`}
+- client에서 오는 정보를 server가 분석할 수 있게 설정
+- client에서 받은 데이터를 비동기 post 방식으로 데이터베이스에 저장
+- server 켜고 postman에서 테스트 - {`success: true`}
