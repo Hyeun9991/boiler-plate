193714690ce910fd7cd8a812fc2a0ff32490ee15
Hyeun9991 <hyeun9991@gmail.com>
2023-05-10 18:16:54 +0900

    hoc 인증 체크 기능 구현

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,4 +1,4 @@
-import { LOGIN_USER, REGISTER_USER } from "../_actions/types";
+import { LOGIN_USER, REGISTER_USER, AUTH_USER } from '../_actions/types';
 
 // reducer: 전 state + 현재 state = next state
 export default function userReducer(state = {}, action) {
@@ -8,6 +8,9 @@ export default function userReducer(state = {}, action) {
 
     case REGISTER_USER:
       return { ...state, register: action.payload };
+
+    case AUTH_USER:
+      return { ...state, userData: action.payload };
 
     default:
       return state;
